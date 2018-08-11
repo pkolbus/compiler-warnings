28dded9bb9619a623286a85f53be534756d8cc7e
Peter Kolbus <peter.kolbus@gmail.com>
2018-08-11 10:15:55 -0500

    Update gcc release tags
    Update gcc tags to 7.3 and 8.2.

--- parsers/process-gcc-git.sh
+++ parsers/process-gcc-git.sh
@@ -63,10 +63,10 @@ parse_gcc_info 5 "$target_dir" "$GIT_DIR"/gcc/{common.opt,c-family/c.opt}
 git -C "$GIT_DIR" checkout gcc-6_4_0-release
 parse_gcc_info 6 "$target_dir" "$GIT_DIR"/gcc/{common.opt,c-family/c.opt}
 
-git -C "$GIT_DIR" checkout gcc-7_2_0-release
+git -C "$GIT_DIR" checkout gcc-7_3_0-release
 parse_gcc_info 7 "$target_dir" "$GIT_DIR"/gcc/{common.opt,c-family/c.opt}
 
-git -C "$GIT_DIR" checkout gcc-8_1_0-release
+git -C "$GIT_DIR" checkout gcc-8_2_0-release
 parse_gcc_info 8 "$target_dir" "$GIT_DIR"/gcc/{common.opt,c-family/c.opt}
 
 git -C "$GIT_DIR" checkout origin/master
