d83050fe12af55242d1214cd88d71e443ddf87d2
Peter Kolbus <peter.kolbus@gmail.com>
2020-12-13 06:55:08 -0600

    feat: Add Apple clang (xcode) warnings
    Add warning lists based on https://github.com/apple/llvm-project, as an
    approximation of available warnings in Xcode.

--- make.sh
+++ make.sh
@@ -8,10 +8,11 @@ print_usage()
     echo "Options:"
     echo " -c, --clang          build clang warning lists"
     echo " -g, --gcc            build gcc warning lists"
+    echo " -x, --xcode          build xcode warning lists"
     echo " -h, --help           display this help"
 }
 
-OPTS=`getopt -o cgh -l clang,gcc,help -- $@`
+OPTS=`getopt -o cgxh -l clang,gcc,xcode,help -- $@`
 if [ $? != 0 ]; then
     print_usage
     exit 1
@@ -19,16 +20,21 @@ fi
 
 eval set -- "$OPTS"
 
-BUILD_CLANG="false"
-BUILD_GCC="false"
+BUILD_CLANG=0
+BUILD_GCC=0
+BUILD_XCODE=0
 while true; do
     case "$1" in
         -c | --clang )
-            BUILD_CLANG="true"
+            BUILD_CLANG=1
             shift
             ;;
         -g | --gcc )
-            BUILD_GCC="true"
+            BUILD_GCC=1
+            shift
+            ;;
+        -x | --xcode )
+            BUILD_XCODE=1
             shift
             ;;
         -h | --help )
@@ -46,9 +52,10 @@ while true; do
     esac
 done
 
-if [ "${BUILD_CLANG}" == "false" ] && [ "${BUILD_GCC}" == "false" ]; then
-    BUILD_CLANG="true"
-    BUILD_GCC="true"
+if [ ${BUILD_CLANG} -eq 0 -a ${BUILD_GCC} -eq 0 -a ${BUILD_XCODE} -eq 0 ]; then
+    BUILD_CLANG=1
+    BUILD_GCC=1
+    BUILD_XCODE=1
 fi
 
 set -e
@@ -108,10 +115,12 @@ run_in_docker flake8 .
 #
 mkdir -p build
 
-if [ "${BUILD_CLANG}" == "true" ]; then
+if [ ${BUILD_CLANG} -eq 1 -o ${BUILD_XCODE} -eq 1 ]; then
     echo "Testing the clang parser..."
     run_in_docker python3 -mdoctest ./parsers/parse-clang-diagnostic-groups.py
+fi
 
+if [ ${BUILD_CLANG} -eq 1 ]; then
     echo "Running the clang parser..."
     CLANG_REMOTE="https://github.com/llvm/llvm-project.git"
 
@@ -125,7 +134,7 @@ if [ "${BUILD_CLANG}" == "true" ]; then
     run_in_docker ./parsers/process-clang-git.sh build/clang
 fi
 
-if [ "${BUILD_GCC}" == "true" ]; then
+if [ ${BUILD_GCC} -eq 1 ]; then
     echo "Running the gcc parser..."
 
     if [ -e build/gcc/.git ]; then
@@ -134,4 +143,18 @@ if [ "${BUILD_GCC}" == "true" ]; then
         run_in_docker git clone git://gcc.gnu.org/git/gcc.git build/gcc
     fi
     run_in_docker ./parsers/process-gcc-git.sh build/gcc
+fi
+
+if [ ${BUILD_XCODE} -eq 1 ]; then
+    echo "Running the xcode parser..."
+    CLANG_REMOTE="https://github.com/apple/llvm-project.git"
+
+    if [ -e build/xcode/.git ]; then
+        run_in_docker git -C build/xcode remote set-url origin ${CLANG_REMOTE}
+        run_in_docker git -C build/xcode remote update
+    else
+        run_in_docker git clone ${CLANG_REMOTE} build/xcode
+    fi
+
+    run_in_docker ./parsers/process-xcode-git.sh build/xcode
 fi
---
+++ parsers/process-xcode-git.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -euo pipefail
+
+DIR=$(dirname "$(readlink -f "$0")")
+
+function parse_clang_info()
+{
+    local version=$1
+    local target_dir=$2
+    local input_dir=$3
+
+    local input_files="${input_dir}/DiagnosticGroups.td"
+
+    local json_file="$target_dir"/warnings-xcode-"$version".json
+
+    llvm-tblgen -dump-json -I "${input_dir}" \
+          "${input_dir}/Diagnostic.td" \
+          | python3 -mjson.tool \
+          > "${json_file}"
+    "$DIR"/parse-clang-diagnostic-groups.py "${json_file}" \
+          > "$target_dir"/warnings-xcode-"$version".txt
+    "$DIR"/parse-clang-diagnostic-groups.py --unique "${json_file}" \
+          > "$target_dir"/warnings-xcode-unique-"$version".txt
+    "$DIR"/parse-clang-diagnostic-groups.py --top-level "${json_file}" \
+          > "$target_dir"/warnings-xcode-top-level-"$version".txt
+    "$DIR"/parse-clang-diagnostic-groups.py --top-level --text "${json_file}" \
+          > "$target_dir"/warnings-xcode-messages-"$version".txt
+}
+
+GIT_DIR=$1
+
+target_dir=$DIR/../xcode
+
+# Parse all apple/stable branches
+versions=( $(git -C "$GIT_DIR" branch --list -r "origin/apple/stable/*" | cut -f4 -d"/") )
+
+mkdir -p "$target_dir"
+
+for v in "${versions[@]}"; do
+    git -C "$GIT_DIR" checkout origin/apple/stable/${v}
+    parse_clang_info ${v} "$target_dir" "$GIT_DIR"/clang/include/clang/Basic
+done
+
+# Parse NEXT (apple/main)
+versions=( "${versions[@]}" "NEXT" )
+
+git -C "$GIT_DIR" checkout origin/apple/main
+parse_clang_info NEXT "$target_dir" "$GIT_DIR"/clang/include/clang/Basic
+
+# Generate diffs
+seq 2 "${#versions[@]}" | while read -r current_version in ; do
+    current=${versions[$(( current_version - 2 ))]}
+    next=${versions[$(( current_version - 1 ))]}
+    "$DIR"/create-diff.sh \
+          "$target_dir"/warnings-xcode-unique-"$current".txt \
+          "$target_dir"/warnings-xcode-unique-"$next".txt \
+          > "$target_dir"/warnings-xcode-diff-"$current-$next".txt
+done
