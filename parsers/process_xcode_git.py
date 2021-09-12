2685d4b383cc1e23cf94700b174ea8bc4ba2b811
Peter Kolbus <peter.kolbus@gmail.com>
2021-09-12 10:28:21 -0500

    chore(xcode): Remove 'xcode' from output names
    Rename warnings-xcode-* to warnings-* to shorten the filenames and
    move toward a common name format for each compiler. process_xcode_git
    now shares parse_clang_info() and create_diffs() from process_clang_git.

--- parsers/process_xcode_git.py
+++ parsers/process_xcode_git.py
@@ -4,63 +4,9 @@ import os
 import sys
 
 import git
-from process_clang_git import format_json, shell
+from process_clang_git import create_diffs, parse_clang_info
 
 DIR = os.path.dirname(os.path.realpath(__file__))
-
-
-def create_diffs(target_dir: str, versions: list[str]) -> None:
-    """
-    Generate diffs for adjacent versions of the 'unique' warning lists.
-
-    :param target_dir: Directory containing files to compare
-    :param versions: List of versions to compare
-    """
-    for version_idx in range(0, len(versions) - 1):
-        current_ver = versions[version_idx]
-        next_ver = versions[version_idx + 1]
-        shell(
-            [
-                f"{DIR}/create-diff.sh",
-                f"{target_dir}/warnings-xcode-unique-{current_ver}.txt",
-                f"{target_dir}/warnings-xcode-unique-{next_ver}.txt",
-            ],
-            f"{target_dir}/warnings-xcode-diff-{current_ver}-{next_ver}.txt",
-        )
-
-
-def parse_clang_info(version: str, target_dir: str, input_dir: str) -> None:
-    """
-    Parse clang diagnostic groups for the given version (in input_dir) to target_dir.
-
-    :param version: Version number to use in output filenames
-    :param target_dir: Directory to write outputs
-    :param input_dir: Directory containing Diagnostic.td file
-    """
-    json_file = f"{target_dir}/warnings-xcode-{version}.json"
-
-    shell(
-        ["llvm-tblgen", "-dump-json", "-I", input_dir, f"{input_dir}/Diagnostic.td"],
-        json_file,
-    )
-    format_json(json_file)
-
-    shell(
-        [f"{DIR}/parse-clang-diagnostic-groups.py", json_file],
-        f"{target_dir}/warnings-xcode-{version}.txt",
-    )
-    shell(
-        [f"{DIR}/parse-clang-diagnostic-groups.py", "--unique", json_file],
-        f"{target_dir}/warnings-xcode-unique-{version}.txt",
-    )
-    shell(
-        [f"{DIR}/parse-clang-diagnostic-groups.py", "--top-level", json_file],
-        f"{target_dir}/warnings-xcode-top-level-{version}.txt",
-    )
-    shell(
-        [f"{DIR}/parse-clang-diagnostic-groups.py", "--top-level", "--text", json_file],
-        f"{target_dir}/warnings-xcode-messages-{version}.txt",
-    )
 
 
 def main() -> None:
